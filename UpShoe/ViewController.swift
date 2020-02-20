a629beb83ac45333d9edb476eddee2056bfb50a9
Jorge Zapata <jorgejaden@gmail.com>
2020-02-20 17:42:45 +0100

    Initial Commit

---
+++ UpShoe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpShoe
+//
+//  Created by JJ Zapata on 2/20/20.
+//  Copyright © 2020 Jorge Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
